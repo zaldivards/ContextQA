11b29b505a42d5d412601e2657a30cbcc1bc9915
Christian Herrera <herrerachristian1897@gmail.com>
2023-06-15 16:38:07 -0600

    Add main bash script

---
+++ run.sh
@@ -0,0 +1,17 @@
+current_path="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+local(){
+    python app.py "$@"
+}
+
+build-docker(){
+    echo "..:: build-docker ::.."
+    docker build -t llm-retriever $current_path
+}
+
+dockerized(){
+    build-docker
+    docker run --rm -v $current_path:/app llm-retriever python app.py "$@"
+}
+
+"$@"
