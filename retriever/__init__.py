7faafd0f5a4e5e04dcbaf31f65078f902c2be730
Christian Herrera <herrerachristian1897@gmail.com>
2023-06-18 12:29:47 -0600

    Update import order to avoid circular dependencies

--- retriever/__init__.py
+++ retriever/__init__.py
@@ -2,12 +2,9 @@ from functools import lru_cache
 
 from pydantic import BaseSettings
 
-from retriever.parsers import models
-from retriever.services import social_media, vector
-
 
 class AppSettings(BaseSettings):
-    open_api_key: str
+    openai_api_key: str
     serpapi_api_key: str
     twitter_api_key: str
     twitter_api_secret: str
@@ -18,3 +15,8 @@ class AppSettings(BaseSettings):
 @lru_cache()
 def settings() -> AppSettings:
     return AppSettings()
+
+
+# pylint: disable=C0413
+from retriever.parsers import models
+from retriever.services import social_media, vector
