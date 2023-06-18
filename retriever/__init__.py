787c08054fd9b96bae0f22c8181a67c640a7cda6
Christian Zaldivar <herrerachristian1897@gmail.com>
2023-06-18 11:39:51 -0600

    App settings (#3)
    * Add `AppSettings` class
    
    * Remove unused module
    
    * Add `AppSettings` usage

--- retriever/__init__.py
+++ retriever/__init__.py
@@ -1,2 +1,20 @@
+from functools import lru_cache
+
+from pydantic import BaseSettings
+
 from retriever.parsers import models
 from retriever.services import social_media, vector
+
+
+class AppSettings(BaseSettings):
+    open_api_key: str
+    serpapi_api_key: str
+    twitter_api_key: str
+    twitter_api_secret: str
+    twitter_access_token: str
+    twitter_api_secret: str
+
+
+@lru_cache()
+def settings() -> AppSettings:
+    return AppSettings()
