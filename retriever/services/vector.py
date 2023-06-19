7f9d2d9279f566a9e47814204b52bd6e4c8c9187
Christian Herrera <herrerachristian1897@gmail.com>
2023-06-19 00:04:25 -0600

    Add `pdf_scan` function

--- retriever/services/vector.py
+++ retriever/services/vector.py
@@ -1,7 +1,10 @@
+from tempfile import NamedTemporaryFile
 from typing import BinaryIO
 
 from langchain import OpenAI, VectorDBQA
+from langchain.chains import RetrievalQA
 from langchain.docstore.document import Document
+from langchain.document_loaders import PyPDFLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Pinecone, SKLearnVectorStore
@@ -53,6 +56,36 @@ def document_scan(params: models.LLMQueryDocumentRequestBody, document: BinaryIO
     """
     splitter = CharacterTextSplitter(separator=params.separator, chunk_size=params.chunk_size, chunk_overlap=0)
     texts = splitter.split_documents([Document(page_content=document.read())])
+    embeddings_util = OpenAIEmbeddings()
+    processor_params = _VECTORSTORE[params.similarity_processor]
+    store = processor_params.clazz.from_documents(texts, embeddings_util, **processor_params.kwargs)
+    finder = VectorDBQA.from_chain_type(OpenAI(), vectorstore=store)
+    result = finder({"query": params.query})
+    return models.VectorScanResult(**result)
+
+
+def pdf_scan(params: models.LLMQueryDocumentRequestBody, document: BinaryIO) -> models.VectorScanResult:
+    """Query the llm providing the best context found in the given document
+
+    Parameters
+    ----------
+    params : models.LLMQueryDocumentRequestBody
+        params from the api request
+    document : BinaryIO
+        document from the api request
+
+    Returns
+    -------
+    models.VectorScanResult
+        result found by the llm given the best context
+    """
+    # the temp file is needed to properly load the pdf file using the `PyPDFLoader` class
+    with NamedTemporaryFile(mode="w") as temp:
+        temp.write(document.read())
+        loader = PyPDFLoader(temp.name)
+        documents = loader.load()
+    splitter = CharacterTextSplitter(separator=params.separator, chunk_size=params.chunk_size, chunk_overlap=0)
+    texts = splitter.split_documents(documents)
     embeddings_util = OpenAIEmbeddings()
     processor_params = _VECTORSTORE[params.similarity_processor]
     store = processor_params.clazz.from_documents(texts, embeddings_util, **processor_params.kwargs)
