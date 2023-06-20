9b68a9e637c34a7b701cf2f482cd9e1c0472fd08
Christian Herrera <herrerachristian1897@gmail.com>
2023-06-19 21:09:18 -0600

    Add `primevue` dependencies

--- gui/src/main.js
+++ gui/src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
 
-createApp(App).mount('#app')
+import 'primevue/resources/themes/saga-blue/theme.css'       //theme
+import 'primevue/resources/primevue.min.css'                 //core css
+import 'primeicons/primeicons.css'
+import '/node_modules/primeflex/primeflex.css'
+
+const app = createApp(App);
+
+app.use(PrimeVue).mount('#app');
