658701fded14d5b6efef73c4d25ce44e4e518b25
Christian Herrera <herrerachristian1897@gmail.com>
2023-06-20 17:36:48 -0600

    Update client and server urls

--- gui/vue.config.js
+++ gui/vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '^/api': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:8080',
         changeOrigin: true,
         logLevel: 'debug',
         pathRewrite: { '^/api': '/' }
