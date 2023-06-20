2a705cfbb5a82d547af7b5858ee897d8d5c376a2
Christian Herrera <herrerachristian1897@gmail.com>
2023-06-20 17:38:24 -0600

    Update `ContextManager`
    Added toast messages and the context request

--- gui/src/views/ContextManager.vue
+++ gui/src/views/ContextManager.vue
@@ -1,61 +1,67 @@
 <template>
-  <form @submit.prevent="setContext" class="w-7 m-5">
-    <FileUpload
-      @remove="() => (this.uploadedFile = null)"
-      accept=".pdf,.txt"
-      :maxFileSize="100000000"
-      @select="handleFileSelect"
-      :showUploadButton="false"
-      :pt="{ badge: { style: 'display: none !important' } }"
-    />
-    <div class="my-4">
-      <div class="flex flex-column gap-2">
-        <label for="username">Separator</label>
-        <InputText
-          class="block mb-2 p-inputtext-lg w-auto max-w-max"
-          v-model="separator"
-          type="text"
-          placeholder="Text separator, default '.'"
-          id="separator"
+  <div>
+    <Toast />
+    <form @submit.prevent="postData" class="w-7 m-5">
+      <div :class="loading ? ['opacity-50', 'disabled'] : ''">
+        <FileUpload
+          @remove="() => (this.uploadedFile = null)"
+          accept=".pdf,.txt"
+          fileLimit="1"
+          :maxFileSize="100000000"
+          @select="handleFileSelect"
+          :showUploadButton="false"
+          :multiple="false"
+          :pt="{ badge: { style: 'display: none !important' } }"
         />
-      </div>
+        <div class="my-4">
+          <div class="flex flex-column gap-2">
+            <label for="username">Separator</label>
+            <InputText
+              class="block mb-2 p-inputtext-lg w-auto max-w-max"
+              v-model="separator"
+              type="text"
+              placeholder="Text separator, default '.'"
+              id="separator"
+            />
+          </div>
 
-      <div class="flex flex-column gap-2">
-        <label for="chunkSize">Chunk size</label>
-        <InputNumber
-          class="block mb-2 p-inputtext-lg w-max"
-          v-model="chunkSize"
-          inputId="integeronly"
-          placeholder="Chunk size, default 200"
-          :min="0"
-          :max="1000"
-          id="chunkSize"
-        />
-      </div>
+          <div class="flex flex-column gap-2">
+            <label for="chunkSize">Chunk size</label>
+            <InputNumber
+              class="block mb-2 p-inputtext-lg w-max"
+              v-model="chunkSize"
+              inputId="integeronly"
+              placeholder="Chunk size, default 200"
+              :min="0"
+              :max="1000"
+              id="chunkSize"
+            />
+          </div>
 
-      <div class="flex flex-column gap-2">
-        <label for="overlap">Overlap</label>
-        <InputNumber
-          class="block outline-none mb-2 p-inputtext-lg w-max"
-          inputId="integeronly"
-          v-model="overlap"
-          placeholder="Chunk overlap, default 0"
-          id="overlap"
-          :min="0"
-          :max="200"
-        />
+          <div class="flex flex-column gap-2">
+            <label for="overlap">Overlap</label>
+            <InputNumber
+              class="block outline-none mb-2 p-inputtext-lg w-max"
+              inputId="integeronly"
+              v-model="overlap"
+              placeholder="Chunk overlap, default 0"
+              id="overlap"
+              :min="0"
+              :max="200"
+            />
+          </div>
+        </div>
       </div>
-    </div>
-
-    <Button
-      type="button"
-      label="Submit"
-      :loading="loading"
-      icon="pi pi-check"
-      @click="setContext"
-      :disabled="nullData"
-    />
-  </form>
+      <Button
+        type="button"
+        label="Submit"
+        icon="pi pi-check"
+        @click="postData"
+        :disabled="nullData"
+        :loading="loading"
+      />
+    </form>
+  </div>
 </template>
 
 <script>
@@ -63,16 +69,19 @@ import FileUpload from "primevue/fileupload";
 import InputText from "primevue/inputtext";
 import InputNumber from "primevue/inputnumber";
 import Button from "primevue/button";
+import Toast from "primevue/toast";
+import { setContext, showSuccess, showError } from "@/utils/client";
 
 export default {
   name: "ContextManager",
-  components: { FileUpload, InputText, Button, InputNumber },
+  components: { FileUpload, InputText, Button, InputNumber, Toast },
   data() {
     return {
       uploadedFile: null,
       separator: ".",
       chunkSize: 200,
       overlap: 0,
+      loading: false,
     };
   },
   computed: {
@@ -86,22 +95,37 @@ export default {
     },
   },
   methods: {
-    setContext() {
-      console.log(
-        this.uploadedFile,
-        this.separator,
-        this.chunkSize,
-        this.overlap
-      );
-      //   console.log(evt.target.files);
+    postData() {
+      this.loading = true;
+      setContext("api/context/set", {
+        separator: this.separator,
+        chunkSize: this.chunkSize,
+        overlap: this.overlap,
+        file: this.uploadedFile,
+      })
+        .then((result) => {
+          console.log(result);
+          showSuccess(
+            "Context set successfully, redirecting to the chat session"
+          );
+          this.loading = false;
+          setTimeout(() => this.$router.push("/chat"), 2000);
+        })
+        .catch((error) => {
+          console.log(error);
+          showError("Something went wrong: " + error.message);
+          this.loading = false;
+        });
     },
     handleFileSelect(evt) {
       this.uploadedFile = evt.files[0];
-      //   this.uploadedFile = evt.target.files[0];
     },
   },
 };
 </script>
 
-<style>
+<style scoped>
+.disabled {
+  pointer-events: none;
+}
 </style>
