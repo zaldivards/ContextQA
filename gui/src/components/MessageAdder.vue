f15928298f9d52d9005908a7ad11f3fbdbb0f74d
Christian Herrera <herrerachristian1897@gmail.com>
2023-06-20 13:31:20 -0600

    Add `MessageAdder` component
    It handles the user questions and emits the corresponding
    events so parent components can continue with the flow

---
+++ gui/src/components/MessageAdder.vue
@@ -0,0 +1,31 @@
+<template>
+  <span class="p-float-label mt-2">
+    <InputText
+      class="mt-2 mb-4"
+      id="question"
+      @keyup.enter="sendQuestion"
+      v-model="question"
+    />
+    <label for="question">Ask me a question</label>
+  </span>
+</template>
+
+<script>
+import InputText from "primevue/inputtext";
+
+export default {
+  name: "MessageAdder",
+  components: { InputText },
+  data() {
+    return { question: "" };
+  },
+  methods: {
+    sendQuestion() {
+      this.$emit("send", this.question);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
