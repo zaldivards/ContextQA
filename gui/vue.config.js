e2e2c1dd59c3220dd92f83ab73538d474cbac011
Christian Herrera <herrerachristian1897@gmail.com>
2023-06-19 22:42:42 -0600

    Add proxy

--- gui/vue.config.js
+++ gui/vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        logLevel: 'debug',
+        pathRewrite: { '^/api': '/' }
+      }
+    }
+  }
+  // lintOnSave: false
 })
