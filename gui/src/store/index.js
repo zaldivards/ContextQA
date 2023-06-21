ea10adea5291cf0a47cbc0409d1a66647065bbbe
Christian Herrera <herrerachristian1897@gmail.com>
2023-06-20 23:40:50 -0600

    Update vuex store

--- gui/src/store/index.js
+++ gui/src/store/index.js
@@ -2,16 +2,40 @@ import { createStore } from 'vuex'
 
 export default createStore({
     state: {
-        identifier: ""
+        identifier: "Test",
+        messages: [],
+        showSpinner: false,
+        lastMessageText: ''
     },
     mutations: {
         updateIdentifier(state, payload) {
             state.identifier = payload;
+        },
+        updateMessages(state, payload) {
+            state.messages.push(payload)
+        },
+        updateFlag(state, payload) {
+            state.showSpinner = payload
+        },
+        updateLastMessage(state, payload) {
+            state.lastMessageText = payload
+            state.messages.at(-1).content = payload
+            state.showSpinner = false
+            console.log(state);
         }
     },
     actions: {
         setIdentifier({ commit }, payload) {
             commit('updateIdentifier', payload);
+        },
+        setMessage({ commit }, payload) {
+            commit('updateMessages', payload);
+        },
+        activateSpinner({ commit }, payload) {
+            commit('updateFlag', payload);
+        },
+        setLastMessage({ commit }, payload) {
+            commit('updateLastMessage', payload);
         }
     }
 });
