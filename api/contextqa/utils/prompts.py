1921de406911df42434bd67cde11565c5bbd2ade
Christian Herrera <herrerachristian1897@gmail.com>
2023-06-29 00:14:13 -0600

    Add `CONTEXTQA_PROMPT`
    This prompt is based on `langchain.chains.conversational_retrieval.prompts.CONDENSE_QUESTION_PROMPT`.
    However, I added an extra description and examples

--- api/contextqa/utils/prompts.py
+++ api/contextqa/utils/prompts.py
@@ -1,20 +1,27 @@
 """Custom Prompt templates"""
 
-from langchain.chains.conversational_retrieval.prompts import (
-    CONDENSE_QUESTION_PROMPT as _CONDENSE_QUESTION_PROMPT,
-)
 from langchain.prompts import PromptTemplate
 
 # part of this template was taken from langchain.chains.conversational_retrieval.prompts
 _template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
 
-Summary of conversation:
-{history}
+If the final message aka the follow up input is a gratitude or goodbye message, that MUST be your final answer
+
+Example 1:
+Assistant: And that is today's wheather
+Human: ok thank you
+Standalone question: Thank you
+
+Example 2:
+Assistant: And that is today's wheather
+Human: ok goodbye
+Standalone question: Goodbye
+
+
 Current conversation:
 {chat_history}
 Follow Up Input: {question}
 Standalone question:"""
 
 
-CONTEXTQA_SUMMARY_TEMPLATE = PromptTemplate.from_template(_template)
-CONTEXTQA_TEMPLATE = _CONDENSE_QUESTION_PROMPT
+CONTEXTQA_PROMPT = PromptTemplate.from_template(_template)
