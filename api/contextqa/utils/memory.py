65e964a3b01b203768fb890c4e376deb4e0a719d
Christian Herrera <herrerachristian1897@gmail.com>
2023-06-29 00:10:14 -0600

    Update `Redis` chat memory
    Now it is a `ConversationBufferWindowMemory` with k messages of 5

--- api/contextqa/utils/memory.py
+++ api/contextqa/utils/memory.py
@@ -1,8 +1,8 @@
 from contextqa import settings
 from langchain.llms import OpenAI
 from langchain.memory import (
-    CombinedMemory,
-    ConversationBufferMemory,
+    ConversationBufferWindowMemory,
+    ConversationSummaryBufferMemory,
     ConversationSummaryMemory,
     RedisChatMessageHistory,
 )
@@ -13,15 +13,31 @@ history_db = RedisChatMessageHistory(session_id="default", url=envs.redis_url)
 
 
 def _redis() -> BaseMemory:
-    return ConversationBufferMemory(chat_memory=history_db)
+    return ConversationBufferWindowMemory(
+        chat_memory=history_db,
+        input_key="question",
+        memory_key="chat_history",
+        max_token_limit=1000,
+        k=5,
+        return_messages=True,
+    )
+
+
+def _summary_memory() -> BaseMemory:
+    return ConversationSummaryMemory(llm=OpenAI(temperature=0), input_key="question")
 
 
 def _redis_with_summary() -> BaseMemory:
-    summary_memory = ConversationSummaryMemory(llm=OpenAI(temperature=0))
-    redis_memory = ConversationBufferMemory(chat_memory=history_db, memory_key="chat_history")
-    memories = CombinedMemory(memories=[summary_memory, redis_memory])
-    return memories
+    memory = ConversationSummaryBufferMemory(
+        llm=OpenAI(temperature=0),
+        input_key="question",
+        memory_key="chat_history",
+        chat_memory=history_db,
+        return_messages=True,
+        max_token_limit=1000,
+    )
+    return memory
 
 
+RedisSummaryMemory = _redis_with_summary
 Redis = _redis
-RedisSummary = _redis_with_summary
