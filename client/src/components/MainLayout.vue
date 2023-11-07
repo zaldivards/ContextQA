ea5e93d6d3905f26858485be290fd8b666f14f1a
Christian Herrera <herrerachristian1897@gmail.com>
2023-11-07 15:35:56 -0600

    Add `MainLayout` component

---
+++ client/src/components/MainLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="container">
+    <aside id="sidebar">
+      <slot name="menu"></slot>
+    </aside>
+    <section id="main">
+      <slot name="main"></slot>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainLayout",
+};
+</script>
+
+<style scoped>
+#container {
+  display: grid;
+  grid-template-columns: 350px 1fr;
+  grid-template-rows: 100vh;
+}
+</style>
