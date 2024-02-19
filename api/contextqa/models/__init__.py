5a805967d14307b2130b977cf1d4433347b33eba
Christian Herrera <herrerachristian1897@gmail.com>
2024-02-19 09:04:36 -0600

    Add `PartialModelData`
    This class will be used to contains a partial
    initialized models and an optional callback

--- api/contextqa/models/__init__.py
+++ api/contextqa/models/__init__.py
@@ -1,10 +1,23 @@
-from typing import TypedDict, Literal
+from dataclasses import dataclass
+from typing import TypedDict, Literal, Callable
+
+from langchain_core.language_models.chat_models import BaseChatModel
+from langchain.callbacks.streaming_aiter_final_only import AsyncFinalIteratorCallbackHandler
 
 
 class SettingsSchema(TypedDict):
     """Dict schema returned from the config manager"""
 
-    platform: Literal["openai", "huggingface"]
+    platform: Literal["openai", "huggingface", "google"]
     model: str
     temperature: float
     local: bool
+    token: str
+
+
+@dataclass
+class PartialModelData:
+    """Contains a partial initialized model and an optional callback"""
+
+    partial_model: Callable[..., BaseChatModel]
+    callback: AsyncFinalIteratorCallbackHandler | None = None
