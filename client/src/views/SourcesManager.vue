b6942fc4b153745f89bca3abad2e2be2120f1afa
Christian Herrera <herrerachristian1897@gmail.com>
2024-02-20 14:25:16 -0600

    Update `SourcesManager` view
    Added empty message

--- client/src/views/SourcesManager.vue
+++ client/src/views/SourcesManager.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="my-2 justify-content-center">
         <Toast class="z-5" />
-        <div class="px-3 lg:px-0 w-full lg:w-9 m-auto grid">
+        <div class="px-3 lg:px-0 w-full lg:w-10 m-auto">
             <h1>Manage sources</h1>
             <DataTable v-model:selection="selectedSources" :value="sources" dataKey="id" tableStyle="min-width: 50rem"
                 paginator size="large" :rows="size" :totalRecords="totalRecords" :rowsPerPageOptions="[5, 10]"
@@ -10,6 +10,7 @@
                 <Column field="title" header="Name"></Column>
                 <Column field="digest" header="Digest"></Column>
                 <Column selectionMode="multiple" headerStyle="width: 3rem"></Column>
+                <template #empty>No sources available</template>
             </DataTable>
 
             <Button type="button" label="Remove selected sources" icon="pi pi-times" severity="danger"
