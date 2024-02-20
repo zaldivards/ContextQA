683c06d561f8e31bfd0cbf93e0cc03301c6d2739
Christian Herrera <herrerachristian1897@gmail.com>
2024-02-20 08:01:23 -0600

    Add `RadioBox` component
    Custom implementation of a styled radio button

---
+++ client/src/components/RadioBox.vue
@@ -0,0 +1,50 @@
+<template>
+    <label :title="capitalizedProvider" class="text-center">
+        <input type="radio" name="provider" :value="provider" v-model="inputValue" :checked="checked" />
+        <img alt="provider logo" :src="src" class="max-w-full max-h-full" />
+    </label>
+</template>
+
+<script>
+export default {
+    name: "RadioBox",
+    props: { provider: String, checked: Boolean, modelValue: String },
+    computed: {
+        src() {
+            return `/images/${this.provider}.png`
+        },
+        capitalizedProvider() {
+            return this.provider.charAt(0).toUpperCase() + this.provider.slice(1);
+        },
+        inputValue: {
+            get() {
+                return this.modelValue
+            },
+            set(value) {
+                this.$emit('update:modelValue', value)
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+input[type="radio"] {
+    position: absolute;
+    opacity: 0;
+}
+
+input[type="radio"]:hover,
+label:hover {
+    cursor: pointer;
+}
+
+input[type="radio"]:checked+img,
+input[type="radio"]:hover+img {
+    filter: grayscale(0%);
+}
+
+img {
+    filter: grayscale(100%);
+}
+</style>
