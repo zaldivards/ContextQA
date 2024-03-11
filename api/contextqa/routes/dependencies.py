444a3403f2a4f01dcb1e2ff1ec4bf99b82982e50
Christian Herrera <herrerachristian1897@gmail.com>
2024-03-11 13:05:21 -0600

    Add routers' dependencies

--- api/contextqa/routes/dependencies.py
+++ api/contextqa/routes/dependencies.py
@@ -1,11 +1,16 @@
 from functools import partial
 from typing import Generator
 
+from chromadb import PersistentClient
 from google.generativeai.types.safety_types import HarmBlockThreshold, HarmCategory
+from langchain.vectorstores.chroma import Chroma
 from langchain_openai import ChatOpenAI
+from pinecone import Pinecone
 
 from contextqa.models import PartialModelData
 from contextqa.services.db import session_factory
+from contextqa.services.context import PineconeManager, LocalManager, BatchProcessor
+from contextqa.utils.clients import StoreClient, PineconeClient, ChromaClient
 from contextqa.utils.settings import get_or_set
 from contextqa.utils.streaming import ChainCompatibleGoogleGenerativeAI
 
@@ -46,6 +51,45 @@ def get_partial_initialized_model() -> PartialModelData:
             ),
             callback=None,
         )
+
+
+def store_client() -> StoreClient:
+    """Return the client for a specific store chosen using the dynamic store settings
+
+    Returns
+    -------
+    StoreClient
+    """
+    store_settings = get_or_set(kind="store")
+    if store_settings["store"] == "chroma":
+        home = str(store_settings["store_params"]["home"])
+        # chroma_client = PersistentClient(path=home)
+        chroma_client = Chroma(
+            client=PersistentClient(path=home),
+            collection_name=store_settings["store_params"]["collection"],
+            persist_directory=home,
+        )
+        return ChromaClient(client=chroma_client)
+    pinecone_client = Pinecone(api_key=store_settings["store_params"]["token"]).Index(
+        store_settings["store_params"]["index"]
+    )
+    return PineconeClient(client=pinecone_client)
+
+
+def context_procesor() -> BatchProcessor:
+    """Return a BatchProcessor for a specific context manager chosen using the dynamic store settings
+
+    Returns
+    -------
+    BatchProcessor
+    """
+    store_settings = get_or_set(kind="store")
+    if store_settings["store"] == "chroma":
+        context_manager = LocalManager()
+    else:
+        context_manager = PineconeManager()
+
+    return BatchProcessor(manager=context_manager)
 
 
 def get_db() -> Generator:
