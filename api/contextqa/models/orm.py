829d8362c6a17403fec086d39bd778980e511030
Christian Herrera <herrerachristian1897@gmail.com>
2024-03-11 01:34:07 -0600

    Add new alembic revision
    The revision adds stores and indexes in order to show the
    existing sources in a specific store's index

--- api/contextqa/models/orm.py
+++ api/contextqa/models/orm.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, DateTime, func, Integer, String
+from sqlalchemy import Column, DateTime, func, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -6,14 +6,38 @@ Base = declarative_base()
 
 
 class ORMBase(Base):
+    """Base ORM"""
+
     __abstract__ = True
 
     created_at = Column(DateTime, default=func.current_timestamp())
 
 
+class VectorStore(ORMBase):
+    """Vector store types"""
+
+    __tablename__ = "store"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100), nullable=False)
+
+
+class Index(ORMBase):
+    """Indexes pertaining to a vector store type"""
+
+    __tablename__ = "index"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100), nullable=False)
+    store_id = Column(Integer, ForeignKey("store.id"), nullable=False)
+
+
 class Source(ORMBase):
+    """Sources ingestes by users"""
+
     __tablename__ = "sources"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(100), nullable=False)
     digest = Column(String(64), nullable=False)
+    index_id = Column(Integer, ForeignKey("index.id"), nullable=False)
