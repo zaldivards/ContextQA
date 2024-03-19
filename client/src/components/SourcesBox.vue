aadb8a188cb04ac9283bc59508a7f417b9c25c15
Christian Herrera <herrerachristian1897@gmail.com>
2024-03-19 15:53:03 -0600

    Add VerticalRow component for CSV format display

--- client/src/components/SourcesBox.vue
+++ client/src/components/SourcesBox.vue
@@ -1,38 +1,41 @@
 <template>
   <div class="w-full grid">
     <p v-if="data.length == 0">No related sources</p>
-    <Card v-else :key="i" v-for="(source, i) in data" class="mx-0 my-5 col-12 lg:col-6" :pt="{
+    <div v-else :key="i" v-for="(source, i) in data" class="mx-0 my-5 col-12 lg:col-6">
+
+
+      <Card :pt="{
       content: { class: 'mx-0' }, title: { class: 'lg:text-xl text-xs' },
-    }">
-      <template #title>
-        {{ source.title }}
-      </template>
-      <template #content>
-        <p v-if="source.format_ == 'txt'">
-          {{ source.content }}
-        </p>
-        <DataTable v-else-if="source.format_ == 'csv'" :value="source.content">
-          <Column v-for="(name, i) in Object.keys(source.content[0])" :key="i" :field="name" :header="name"></Column>
-        </DataTable>
-        <div v-else class="text-center">
-          <Image class="m-0" :src="`data:image/jpg;base64,${source.content}`" preview
-            :pt="{ image: { class: 'w-full' }, mask: { class: 'w-full' } }" />
-        </div>
-      </template>
-    </Card>
+    }" class="h-full">
+        <template #title>
+          {{ source.title }}
+        </template>
+        <template #content>
+          <p v-if="source.format_ == 'txt'">
+            {{ source.content }}
+          </p>
+
+          <VerticalRow v-else-if="source.format_ == 'csv'" :cell="source.content[0]" />
+
+          <div v-else class="text-center">
+            <Image class="m-0" :src="`data:image/jpg;base64,${source.content}`" preview
+              :pt="{ image: { class: 'w-full' }, mask: { class: 'w-full' } }" />
+          </div>
+        </template>
+      </Card>
+    </div>
   </div>
 </template>
 
 <script>
 import Card from "primevue/card";
-import DataTable from "primevue/datatable";
-import Column from "primevue/column";
 import Image from "primevue/image";
+import VerticalRow from "@/components/VerticalRow.vue"
 
 export default {
   inject: ["dialogRef"],
   name: "SourcesBox",
-  components: { Card, DataTable, Column, Image },
+  components: { Card, Image, VerticalRow },
   data() {
     return {
       data: [],
---
+++ client/src/components/VerticalRow.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <table>
+            <tbody>
+                <tr :key="i" v-for="([name, value], i) in Object.entries(cell)" class="cursor-pointer">
+                    <th class="text-left border-bottom-1 border-black-alpha-40">{{ capitalize(name) }}</th>
+                    <td class="py-1 border-bottom-1 border-black-alpha-40">{{ value }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "VerticalRow",
+    props: { cell: Object },
+    methods: {
+        capitalize(value) {
+            return value.charAt(0).toUpperCase() + value.substring(1)
+        }
+    }
+}
+</script>
+
+<style scoped>
+table,
+tbody,
+th,
+td {
+    border-collapse: collapse;
+}
+
+tr:hover td,
+tr:hover th {
+    border-bottom-color: #6dd3c8 !important;
+}
+</style>
