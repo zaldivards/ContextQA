4120e334198db01bcd641c025f59ade32a62ce40
Christian Herrera <herrerachristian1897@gmail.com>
2024-05-30 01:38:51 -0600

    fix: RadioBox component for dynamic image filter
    Introduce hover effect and adjust image grayscale filter
    dynamically based on hover and check state.
    
    The CSS approach was broken when multiple groups
    existed in the same view

--- client/src/components/RadioBox.vue
+++ client/src/components/RadioBox.vue
@@ -1,5 +1,8 @@
 <template>
-    <label :title="capitalizedProvider" class="text-center border-round-2xl flex flex-column justify-content-center align-items-center" :class="checked ? 'checked shadow-4' : ''">
+    <label :title="capitalizedProvider"
+        class="text-center border-round-2xl flex flex-column justify-content-center align-items-center"
+        :class="checked ? 'checked shadow-4' : ''" @mouseover="hover = true" @mouseleave="hover = false"
+        :style="{ filter: imgFilter }">
         <input type="radio" name="provider" :value="provider" v-model="inputValue" :checked="checked" />
         <img alt="provider logo" :src="src" class="max-w-full max-h-full" />
     </label>
@@ -8,6 +11,11 @@
 <script>
 export default {
     name: "RadioBox",
+    data() {
+        return {
+            hover: false
+        };
+    },
     props: { provider: String, checked: Boolean, modelValue: String },
     computed: {
         src() {
@@ -23,6 +31,9 @@ export default {
             set(value) {
                 this.$emit('update:modelValue', value)
             }
+        },
+        imgFilter() {
+            return this.checked || this.hover ? 'grayscale(0%)' : 'grayscale(100%)';
         }
     }
 }
@@ -43,17 +54,7 @@ label {
     background-color: rgba(252, 254, 255, 0);
 }
 
-
-input[type="radio"]:checked+img,
-input[type="radio"]:hover+img {
-    filter: grayscale(0%);
-}
-
 .checked {
     background-color: #10223f
-}
-
-img {
-    filter: grayscale(100%);
 }
 </style>
