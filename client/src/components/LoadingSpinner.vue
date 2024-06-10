828d3fb5b5441cdf4cfc0978c43d8ccd5770f193
Christian Herrera <herrerachristian1897@gmail.com>
2024-06-10 00:52:26 -0600

    feat: LoadingSpinner component for dynamic loading feedback

---
+++ client/src/components/LoadingSpinner.vue
@@ -0,0 +1,22 @@
+<template>
+    <div :style="{ height: size, width: size, borderTopColor: 'blue' }"
+        class="animate-spin border-1 border-circle" />
+</template>
+
+<script>
+export default {
+    name: 'LoadingSpinner',
+    props: { size: String }
+}
+</script>
+
+<style scoped>
+@keyframes spin {
+    to {
+        transform: rotate(360deg);
+    }
+}
+.animate-spin {
+    animation: spin 1s linear infinite;
+}
+</style>
