3146cb8ddf85296d843a199b27360d81fd3b31b7
Christian Herrera <herrerachristian1897@gmail.com>
2024-07-05 17:24:09 -0600

    feat: add setup script

---
+++ api/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ContextQA",
+    version="{{VERSION_PLACEHOLDER}}",
+    author="Christian Zaldivar",
+    author_email="herrerachristian1897@gmail.com",
+    maintainer="Christian Zaldivar",
+    maintainer_email="herrerachristian1897@gmail.com",
+    description="Chat with your data by leveraging the power of LLMs and vector databases",
+    license="MIT",
+    url="https://github.com/zaldivards/ContextQA/",
+    keywords=["OpenAI", "LLM", "LLM client", "LLM app", "QA", "Agent", "LLM agent"],
+    python_requires=">=3.11",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=open("requirements.txt", encoding="utf-8").readlines(),
+    classifiers=[
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.11",
+        "Framework :: FastAPI",
+    ],
+    entry_points={"console_scripts": ["contextqa = contextqa.cli:main"]},
+)
