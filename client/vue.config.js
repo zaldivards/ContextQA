ab7202cc882d459ea5086aa52e8cc3b501fc5f53
Christian Herrera <herrerachristian1897@gmail.com>
2024-07-08 02:29:49 -0600

    fix: update output dir of client build
    The client build output directory was not correct

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 const path = require("path");
 module.exports = defineConfig({
-  outputDir: path.resolve(__dirname, "../api/ui"),
+  outputDir: path.resolve(__dirname, "../api/contextqa/ui"),
   transpileDependencies: true
 })
